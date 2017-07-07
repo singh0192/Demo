726fa1d5c2519a240ea6f737c263c52314144f65
HOME-PC <HOME-PC@DESKTOP>
2017-07-07 22:42:05 +0530

    From workspace 2

--- DemoGit/src/com/demo/Demo.java
+++ DemoGit/src/com/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Push Config");
+System.out.println("from workspace 2");
 	}
 
 }
