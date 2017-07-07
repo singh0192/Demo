061a98dfb443f57c31dd22f17bee011780f08e59
HOME-PC <HOME-PC@DESKTOP>
2017-07-07 22:36:05 +0530

    sadfsad

--- DemoGit/src/com/demo/Demo.java
+++ DemoGit/src/com/demo/Demo.java
@@ -5,7 +5,6 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Push Config");
-		System.out.println("pull Config");
 	}
 
 }
